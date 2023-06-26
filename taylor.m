0209ee9994d1818457db9c66f22e26c5313823d3
Mauricio Apaza <mauricioapaza@MacBook-Pro-de-Mauricio.local>
2023-06-25 22:19:49 -0500

    Cambios Finales

--- taylor.m
+++ taylor.m
@@ -1,11 +1,11 @@
 function [t,y] = taylor(t0,y0,T,p,f,dy,dt)
     h = T/p;
-    t = zeros(1,p+1);
-    y = zeros(1,p+1);
+    t = zeros(p+1,1);
+    y = zeros(p+1,1);
     t(1) = t0;
     y(1) = y0;
     for n = 2:p+1
         t(n) = t(n-1) + h;
-        y(n) = y(n-1) + f(t(n-1), y(n-1))*h + (dt(t(n-1), y(n-1)) + dy(t(n-1),y(n-1)) + f(t(n-1), y(n-1)))/2*h^2;
+        y(n) = y(n-1) + f(t(n-1), y(n-1))*h + ((dt(t(n-1), y(n-1)) + dy(t(n-1),y(n-1))*f(t(n-1), y(n-1)))/2)*h^2;
     end
 end
